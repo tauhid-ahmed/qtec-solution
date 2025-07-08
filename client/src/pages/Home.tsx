9cccd383576977f95eedb3c05b8ec425d55115d8
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-08 10:22:33 +0600

    refactor: homepage container logic

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,47 @@
+import { useProductContext } from "@/contexts/product-context";
+import { useEffect, useState } from "react";
+import Products from "../components/Products";
+import { ProductCardSkeleton } from "@/components/ProductCard";
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  const [products, setProducts] = useState([]);
+  const { state, fetching, fetchSuccess, fetchError } = useProductContext();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      fetching();
+      try {
+        const response = await fetch(
+          "https://qtec-solution.onrender.com/products"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const products = await response.json();
+        setProducts(products);
+        fetchSuccess();
+      } catch (error) {
+        if (error instanceof Error) {
+          fetchError(error.message);
+        }
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  if (state.status === "fetching") {
+    return (
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-4 mt-4">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCardSkeleton key={index} />
+        ))}
+      </div>
+    );
+  }
+
+  if (state.status === "error") {
+    return <div>{state.message}</div>;
+  }
+
+  return <Products products={products} />;
 }
