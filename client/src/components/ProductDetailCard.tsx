a5aa017e6421b4a65db10dfae8a165e0bd0f4fec
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-08 10:24:19 +0600

    feature: product details page add

---
+++ client/src/components/ProductDetailCard.tsx
@@ -0,0 +1,147 @@
+import type { Product } from "@/types";
+import AddToCartButton from "./AddToCartButton";
+import Link from "./Link";
+import {
+  LucideEye,
+  LucideHeart,
+  LucideMinus,
+  LucideMoveLeft,
+  LucidePlus,
+  LucideShare2,
+  LucideStar,
+} from "lucide-react";
+import { Button } from "./ui/button";
+import { Skeleton } from "./ui/skeleton";
+import { Badge } from "./ui/badge";
+import { Separator } from "./ui/separator";
+import { useProductContext } from "@/contexts/product-context";
+import { useEffect } from "react";
+
+type ProductDetailCardProps = {
+  product: Product;
+};
+
+export default function ProductDetailCard({ product }: ProductDetailCardProps) {
+  const { state, increaseQuantity, decreaseQuantity, productView } =
+    useProductContext();
+
+  const cartProduct = Object.values(state.cart).find(
+    (item) => item.id === product.id
+  );
+
+  useEffect(() => {
+    productView();
+  }, []);
+
+  return (
+    <div className="mt-4">
+      <Button variant="link">
+        <Link className="flex items-center gap-2" href="/">
+          <LucideMoveLeft /> Back to products
+        </Link>
+      </Button>
+      <div className="flex flex-col sm:flex-row flex-wrap gap-8 justify-between mt-4">
+        <div className="flex-1 items-center justify-center rounded-2xl shadow p-8 bg-secondary">
+          <img
+            className="max-h-96 aspect-square mx-auto object-cover"
+            src={product.image}
+            alt={product.title}
+          />
+        </div>
+        <div className="flex-1 space-y-6">
+          <div className="flex justify-between gap-2">
+            <Badge variant="secondary" className="capitalize">
+              {product.category}
+            </Badge>
+            <Badge variant="outline">
+              <LucideEye />
+              {product.views} views
+            </Badge>
+          </div>
+          <div className="space-y-6">
+            <div className="space-y-4">
+              <h2 className="text-3xl font-semibold">{product.title}</h2>
+              <div className="flex gap-2 items-center">
+                <span className="flex text-amber-500">
+                  <LucideStar size="20" />
+                  <LucideStar size="20" />
+                  <LucideStar size="20" />
+                  <LucideStar size="20" />
+                  <LucideStar size="20" />
+                </span>
+                <strong>{product.rating?.rate}</strong>
+                <span className="text-sm text-muted-foreground">
+                  ({product.rating?.count} reviews)
+                </span>
+              </div>
+              <span className="text-3xl font-medium block text-blue-500">
+                ${product.price?.toFixed(2)}
+              </span>
+            </div>
+            <Separator />
+            <div className="">
+              <h4 className="font-semibold text-lg">Description</h4>
+              <p className="mt-2">{product.description}</p>
+            </div>
+            <Separator />
+            <div className="flex items-center gap-2">
+              <span className="font-semibold">Quantity:</span>
+              <div className="flex items-center border rounded-xl overflow-hidden">
+                <Button
+                  onClick={() => decreaseQuantity(product)}
+                  variant="ghost"
+                  size="icon"
+                >
+                  <LucideMinus />
+                </Button>
+                <span className="px-4 py-2">{cartProduct?.quantity || 0}</span>
+                <Button
+                  onClick={() => increaseQuantity(product)}
+                  variant="ghost"
+                  size="icon"
+                >
+                  <LucidePlus />
+                </Button>
+              </div>
+            </div>
+            <div className="flex gap-2 mt-4">
+              <AddToCartButton className="flex-1" product={product} />
+              <Button variant="secondary">
+                <LucideHeart />
+              </Button>
+              <Button variant="secondary">
+                <LucideShare2 />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function ProductDetailSkeleton() {
+  return (
+    <div className="mt-4">
+      <Button variant="link">
+        <Link className="flex items-center gap-2" href="/">
+          <LucideMoveLeft /> Back to products
+        </Link>
+      </Button>
+      <div className="flex flex-col sm:flex-row flex-wrap gap-8 justify-between mt-4">
+        <div className="flex-1">
+          <Skeleton className="h-96" />
+        </div>
+
+        <div className="flex-1 space-y-4">
+          <Skeleton className="h-10" />
+          <Skeleton className="h-14 w-40" />
+          <Skeleton className="h-10 w-32" />
+          <Skeleton className="h-32" />
+          <Skeleton className="h-10" />
+          <Skeleton className="h-8 w-24" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/ProductDetail.tsx
+++ client/src/pages/ProductDetail.tsx
@@ -1,3 +1,51 @@
+import ProductDetailCard, {
+  ProductDetailSkeleton,
+} from "@/components/ProductDetailCard";
+import { useRouter } from "@/lib/router";
+import { useProductContext } from "@/contexts/product-context";
+import type { Product } from "@/types";
+import { useEffect, useState } from "react";
+
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  const [product, setProduct] = useState({});
+  const { param } = useRouter();
+  const { state, fetching, fetchSuccess, fetchError } = useProductContext();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      fetching();
+      try {
+        const response = await fetch(
+          `https://qtec-solution.onrender.com/product/${param}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const products = await response.json();
+        setProduct(products);
+        fetchSuccess();
+      } catch (error) {
+        if (error instanceof Error) {
+          fetchError(error.message);
+        }
+      }
+    };
+    if (param) {
+      fetchProducts();
+    }
+  }, [param]);
+
+  if (state.status === "initial" || state.status === "fetching") {
+    return <ProductDetailSkeleton />;
+  }
+
+  if (state.status === "error") {
+    return <div>Error: {state.message}</div>;
+  }
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return <ProductDetailCard product={product as Product} />;
 }
