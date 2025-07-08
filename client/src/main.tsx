3994c07bfba3be4dfd47a26927d43e75886a32a3
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-08 10:25:51 +0600

    refactor: main component add pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "./lib/router";
-import App from "./App.tsx";
+import App from "./App";
+import { ProductProvider } from "./contexts/product-context";
+import Header from "./components/Header";
+import Container from "./components/Container";
+import CheckoutForm from "./components/CheckoutForm";
+import { Toaster } from "@/components/ui/sonner";
+import PlaceOrderAlert from "./components/PlaceOrderAlert";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider>
-      <App />
+      <ProductProvider>
+        <Header />
+        <Container>
+          <App />
+        </Container>
+        <Toaster />
+        <CheckoutForm />
+        <PlaceOrderAlert />
+      </ProductProvider>
     </RouterProvider>
   </StrictMode>
 );
